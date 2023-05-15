e6fde3d7be3a0f434d353417cbe8bd270f7eb38f
faith catherine <43315340+faithkatherine@users.noreply.github.com>
2023-05-15 16:49:50 +0300

    gcloud deployment

--- HotelRoomListing/settings.py
+++ HotelRoomListing/settings.py
@@ -127,10 +127,14 @@ USE_I18N = True
 USE_TZ = True
 
 
+GOOGLE_APPLICATION_CREDENTIALS = os.path.join(
+    BASE_DIR, os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- Rooms/models.py
+++ Rooms/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Rooms(models.Model):
+    room_number     =models.CharField(max_length=50)
+    
